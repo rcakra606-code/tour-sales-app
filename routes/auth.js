e26702df7bb53f789bce0521eefb262013cd9ad4
rcakra606-code <rcakra606@gmail.com>
2025-10-11 23:39:18 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
-// =====================================
-// ✅ Auth Routes
-// =====================================
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// Endpoint registrasi
 router.post("/register", authController.register);
+
+// Endpoint login
 router.post("/login", authController.login);
+
+// Endpoint verifikasi token
 router.get("/verify", authController.verifyToken);
 
 module.exports = router;
