c4c06fc2fc26a5e4a15e16ddc5a54a3e6d6d30fa
rcakra606-code <rcakra606@gmail.com>
2025-10-13 22:20:59 +0700

    Update tours.js

--- routes/tours.js
+++ routes/tours.js
@@ -1,12 +1,14 @@
+// routes/tours.js
 const express = require("express");
 const router = express.Router();
 const tourController = require("../controllers/tourController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-// Semua endpoint di bawah ini wajib token JWT
+// Semua route di bawah ini wajib login
 router.use(authMiddleware);
 
 router.get("/", tourController.getAllTours);
+router.get("/:id", tourController.getTourById);
 router.post("/", tourController.createTour);
 router.put("/:id", tourController.updateTour);
 router.delete("/:id", tourController.deleteTour);
