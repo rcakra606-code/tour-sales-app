bc144319b66850a9f9c0047ae1c5368cabef1467
rcakra606-code <rcakra606@gmail.com>
2025-10-14 00:13:08 +0700

    Update sales.js

--- public/js/sales.js
+++ public/js/sales.js
@@ -1,12 +1,18 @@
-window.sales = {
-  loadSales: async function () {
-    try {
-      const token = localStorage.getItem("token");
-      const data = await window.api.request("/api/sales", "GET", null, token);
-      console.log("Sales loaded:", data.sales);
-      // TODO: render ke table atau chart
-    } catch (err) {
-      window.auth.showError(err.message);
-    }
+window.Sales = {
+  loadSales: async function() {
+    const data = await window.Api.get("/sales");
+    return data.sales || [];
+  },
+
+  createSale: async function(sale) {
+    return await window.Api.post("/sales", sale);
+  },
+
+  updateSale: async function(id, sale) {
+    return await window.Api.put(`/sales/${id}`, sale);
+  },
+
+  deleteSale: async function(id) {
+    return await window.Api.delete(`/sales/${id}`);
   }
 };
