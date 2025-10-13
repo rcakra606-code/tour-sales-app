9a1ef92f6127838a650b3280f5502283834833cc
rcakra606-code <rcakra606@gmail.com>
2025-10-14 01:26:56 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,18 @@
+// middleware/authMiddleware.js
 const jwt = require("jsonwebtoken");
 
 module.exports = function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer "))
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Akses ditolak. Token tidak ada." });
+  }
 
   const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretkey");
-    req.user = decoded; // {id, username, role}
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(403).json({ message: "Token tidak valid." });
