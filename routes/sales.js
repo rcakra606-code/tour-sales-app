3c50df018cee51497c31c599141ab6923de1f555
rcakra606-code <rcakra606@gmail.com>
2025-10-13 22:18:21 +0700

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const salesController = require("../controllers/salesController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-// Semua route sales hanya bisa diakses setelah login
 router.use(authMiddleware);
 
-// Tambahkan route
 router.get("/", salesController.getAllSales);
 router.post("/", salesController.createSale);
 router.put("/:id", salesController.updateSale);
