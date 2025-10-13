1184d36b6caffe5bb4f571bc5c9dd09e2b04e967
rcakra606-code <rcakra606@gmail.com>
2025-10-13 22:21:09 +0700

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -1,11 +1,14 @@
+// routes/sales.js
 const express = require("express");
 const router = express.Router();
 const salesController = require("../controllers/salesController");
 const authMiddleware = require("../middleware/authMiddleware");
 
+// Proteksi semua route pakai JWT
 router.use(authMiddleware);
 
 router.get("/", salesController.getAllSales);
+router.get("/:id", salesController.getSaleById);
 router.post("/", salesController.createSale);
 router.put("/:id", salesController.updateSale);
 router.delete("/:id", salesController.deleteSale);
