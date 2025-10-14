62fc703f284661a1ccffa90a3c6f32332f639aee
rcakra606-code <rcakra606@gmail.com>
2025-10-14 22:59:29 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,22 @@
 const jwt = require("jsonwebtoken");
-const SECRET = "travel_secret_key";
+const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
 
-exports.verifyToken = (req, res, next) => {
+function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Token tidak ditemukan" });
+  if (!token) return res.status(401).json({ success: false, message: "Token tidak ditemukan" });
 
-  jwt.verify(token, SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Token tidak valid" });
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ success: false, message: "Token tidak valid" });
     req.user = user;
     next();
   });
-};
+}
+
+function isAdmin(req, res, next) {
+  if (req.user.role !== "admin")
+    return res.status(403).json({ success: false, message: "Akses ditolak (admin only)" });
+  next();
+}
+
+module.exports = { authenticateToken, isAdmin };
