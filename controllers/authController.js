beea36b413c8f6e483040cf6bd10e08c9bbc0007
rcakra606-code <rcakra606@gmail.com>
2025-10-14 22:53:16 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const db = require("../config/database");
 
 const SECRET = "travel_secret_key";
