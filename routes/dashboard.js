1e62e3a40c87ba8415ae9e6c797d404fd837194b
rcakra606-code <rcakra606@gmail.com>
2025-10-14 23:03:56 +0700

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authenticateToken } = require("../middleware/authMiddleware");
-const db = require("../config/db");
+const db = require("../config/database");
 
 // === Dashboard Summary ===
 router.get("/", authenticateToken, async (req, res) => {
