6d83b79a62fd71bbdefc8281089da9d4dbb8edb9
rcakra606-code <rcakra606@gmail.com>
2025-10-14 22:43:59 +0700

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../middlewares/authMiddleware");
+const { verifyToken } = require("../middleware/authMiddleware");
 const { getAllSales, createSale } = require("../controllers/salesController");
 
 router.get("/", verifyToken, getAllSales);
