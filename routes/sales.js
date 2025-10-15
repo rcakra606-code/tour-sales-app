d9688c79ad178e0c3849d3fe6840679998fd1851
rcakra606-code <rcakra606@gmail.com>
2025-10-15 16:07:37 +0700

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -1,9 +1,15 @@
+// routes/sales.js
 const express = require("express");
 const router = express.Router();
 const salesController = require("../controllers/salesController");
 const roleCheck = require("../middleware/roleCheck");
 
-router.get("/", salesController.getSales);
-router.post("/", roleCheck("super", "semi"), salesController.addSale);
+// 🔹 Semua user bisa lihat sales list
+router.get("/", salesController.getAllSales);
+
+// 🔹 Hanya super dan semi yang bisa menambah atau mengubah data sales
+router.post("/", roleCheck("super", "semi"), salesController.createSale);
+router.put("/:id", roleCheck("super", "semi"), salesController.updateSale);
+router.delete("/:id", roleCheck("super"), salesController.deleteSale);
 
 module.exports = router;
