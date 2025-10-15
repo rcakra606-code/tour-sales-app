0337d87a5f99450f95c1c95f2502e8374322eec1
rcakra606-code <rcakra606@gmail.com>
2025-10-15 16:07:45 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
+// routes/users.js
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const usersController = require("../controllers/usersController");
 const roleCheck = require("../middleware/roleCheck");
 
-router.get("/", roleCheck("super"), userController.getAllUsers);
-router.post("/", roleCheck("super"), userController.createUser);
+// 🔹 Hanya super bisa lihat dan kelola user
+router.get("/", roleCheck("super"), usersController.getAllUsers);
+router.post("/", roleCheck("super"), usersController.createUser);
+router.put("/:id", roleCheck("super"), usersController.updateUser);
+router.delete("/:id", roleCheck("super"), usersController.deleteUser);
 
 module.exports = router;
