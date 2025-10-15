6dd71896f96c5105ea1f15c9b105cf1ca3324599
rcakra606-code <rcakra606@gmail.com>
2025-10-15 16:06:06 +0700

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const salesController = require("../controllers/salesController");
-const auth = require("../middleware/authMiddleware");
 const roleCheck = require("../middleware/roleCheck");
-router.post("/", auth, salesController.create);
-router.get("/", auth, salesController.list);
-router.get("/targets", auth, salesController.targets);
-router.post("/targets", auth, roleCheck(["super","semi"]), salesController.setTarget);
+
+router.get("/", salesController.getSales);
+router.post("/", roleCheck("super", "semi"), salesController.addSale);
+
 module.exports = router;
