4b2ad81041f33088e6772a28a88714a523bcc82c
rcakra606-code <rcakra606@gmail.com>
2025-10-16 15:54:15 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 // routes/users.js
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
+const roleCheck = require("../middleware/roleCheck");
+const usersController = require("../controllers/userController");
 
+// Semua route butuh login
 router.use(authMiddleware);
 
-router.get("/", usersController.getAllUsers);
+// Admin only
+router.get("/", roleCheck("admin"), usersController.getAllUsers);
+router.put("/reset/:id", roleCheck("admin"), usersController.resetPassword);
+
+// User biasa bisa ubah password sendiri
+router.put("/change-password", usersController.changePassword);
 
 module.exports = router;
