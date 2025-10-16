30a4838d73833d63d573b87a5d4db5a7fe0ff175
rcakra606-code <rcakra606@gmail.com>
2025-10-16 12:36:33 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,24 +3,9 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
-const roleCheck = require("../middleware/roleCheck");
 
-// Semua route user harus login
 router.use(authMiddleware);
 
-// === Ambil semua user (khusus super) ===
-router.get("/", roleCheck("super"), usersController.getAllUsers);
-
-// === Tambah user baru (khusus super) ===
-router.post("/", roleCheck("super"), usersController.addUser);
-
-// === Ambil 1 user berdasarkan ID (super & semi) ===
-router.get("/:id", roleCheck("super", "semi"), usersController.getUserById);
-
-// === Update user (super atau user sendiri) ===
-router.put("/:id", roleCheck("super", "semi"), usersController.updateUser);
-
-// === Hapus user (hanya super) ===
-router.delete("/:id", roleCheck("super"), usersController.deleteUser);
+router.get("/", usersController.getAllUsers);
 
 module.exports = router;
