165d22e7e5f2351a5deb1eb6cd02f7555347aa0c
rcakra606-code <rcakra606@gmail.com>
2025-10-16 11:41:18 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 // routes/users.js
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controllers/usersController");
+const usersController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleCheck = require("../middleware/roleCheck");
 
