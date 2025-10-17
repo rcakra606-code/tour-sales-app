afa92d6f8143ae35ec7f20cacdd3e6d0b9d2581d
rcakra606-code <rcakra606@gmail.com>
2025-10-17 16:38:19 +0700

    Update regions.js

--- routes/regions.js
+++ routes/regions.js
@@ -1,17 +1,13 @@
-// routes/regions.js
 const express = require("express");
 const router = express.Router();
 const regionController = require("../controllers/regionController");
 const authMiddleware = require("../middleware/authMiddleware");
-const roleCheck = require("../middleware/roleCheck");
 
-// Semua endpoint region butuh login
 router.use(authMiddleware);
 
-// Hanya admin (super) dan semi-admin yang boleh ubah data
 router.get("/", regionController.getAll);
-router.post("/", roleCheck(["super", "semi"]), regionController.create);
-router.put("/:id", roleCheck(["super", "semi"]), regionController.update);
-router.delete("/:id", roleCheck(["super"]), regionController.remove);
+router.post("/", regionController.create);
+router.put("/:id", regionController.update);
+router.delete("/:id", regionController.delete);
 
 module.exports = router;
