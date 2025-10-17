2bd2dcfb358cae54191db37865a41e700a0a448f
rcakra606-code <rcakra606@gmail.com>
2025-10-17 19:18:28 +0700

    Update region.js

--- routes/region.js
+++ routes/region.js
@@ -1,15 +1,15 @@
 // routes/regions.js
 const express = require("express");
 const router = express.Router();
-const regionsController = require("../controllers/regionController");
+const regionsController = require("../controllers/regionsController");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleCheck = require("../middleware/roleCheck");
 
 router.use(authMiddleware);
 
-router.get("/", regionController.getAll);
-router.post("/", roleCheck(["admin"]), regionController.create);
-router.put("/:id", roleCheck(["admin"]), regionController.update);
-router.delete("/:id", roleCheck(["admin"]), regionController.delete);
+router.get("/", regionsController.getAll);
+router.post("/", roleCheck(["admin"]), regionsController.create);
+router.put("/:id", roleCheck(["admin"]), regionsController.update);
+router.delete("/:id", roleCheck(["admin"]), regionsController.delete);
 
 module.exports = router;
