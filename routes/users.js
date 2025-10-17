0e925ea9b4d5a5b75190315409bf8f0446a4464f
rcakra606-code <rcakra606@gmail.com>
2025-10-17 13:49:05 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,21 @@
 // routes/users.js
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controllers/userController");
+const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleCheck = require("../middleware/roleCheck");
 
-// Semua route ini butuh login
 router.use(authMiddleware);
 
-// === Admin & Super ===
-router.get("/", roleCheck(["super", "semi"]), usersController.getAllUsers);
-router.post("/", roleCheck(["super", "semi"]), usersController.createUser);
-router.put("/reset/:id", roleCheck(["super", "semi"]), usersController.resetPassword);
-router.delete("/:id", roleCheck(["super"]), usersController.deleteUser);
+// === CRUD dasar ===
+router.get("/", roleCheck(["super", "semi"]), userController.getAll);
+router.get("/:id", roleCheck(["super", "semi"]), userController.getById);
+router.post("/", roleCheck(["super"]), userController.create);
+router.put("/:id", roleCheck(["super"]), userController.update);
+router.delete("/:id", roleCheck(["super"]), userController.remove);
 
-// === User Biasa ===
-router.put("/change-password", usersController.changePassword);
+// === Password Management ===
+router.post("/change-password", userController.changePassword); // user ganti sendiri
+router.post("/:id/reset-password", roleCheck(["super"]), userController.resetPassword); // admin reset user
 
 module.exports = router;
