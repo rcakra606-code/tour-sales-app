59ba0f5e5f4ceecc347d1e538b22ccfb59b296f9
rcakra606-code <rcakra606@gmail.com>
2025-10-18 00:02:53 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
+// routes/auth.js
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/login", authController.login);
-router.get("/verify", authController.verifyToken);
-router.post("/refresh", authController.refreshToken);
+router.post("/refresh", authController.refresh);
+router.get("/verify", authController.verify);
 router.post("/logout", authController.logout);
 
 module.exports = router;
