d83b08ce91985c4f673e84cbc2fe70cd9fec5e7b
rcakra606-code <rcakra606@gmail.com>
2025-10-17 23:36:55 +0700

    Update tours.js

--- routes/tours.js
+++ routes/tours.js
@@ -1,10 +1,20 @@
+// routes/tours.js
 const express = require("express");
 const router = express.Router();
 const tourController = require("../controllers/tourController");
 
+// GET list & summary
 router.get("/", tourController.getTours);
-router.post("/", tourController.createTour);
-router.delete("/:id", tourController.deleteTour);
 router.get("/summary", tourController.getTourSummary);
+
+// POST new tour
+router.post("/", tourController.createTour);
+
+// DELETE
+router.delete("/:id", tourController.deleteTour);
+
+// EXPORT routes
+router.get("/export/excel", tourController.exportToursExcel);
+router.get("/export/csv", tourController.exportToursCSV);
 
 module.exports = router;
