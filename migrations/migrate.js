eaf7db77f13481af5087381c6696e7bd1f512274
rcakra606-code <rcakra606@gmail.com>
2025-10-18 11:57:06 +0700

    Delete migrations directory

--- migrations/migrate.js
+++
@@ -1,8 +0,0 @@
-const Database = require("better-sqlite3");
-const db = new Database("./data/travel.db");
-
-db.exec(`
-ALTER TABLE users ADD COLUMN last_login TEXT;
-`);
-
-console.log("✅ Migration complete.");
