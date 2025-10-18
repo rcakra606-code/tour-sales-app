c4562a5f90fbfe601fa905f7286028046a017b1b
rcakra606-code <rcakra606@gmail.com>
2025-10-18 16:27:46 +0700

    Delete public/js/config.js

--- public/js/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const API_BASE = "/api";
-const TOKEN = localStorage.getItem("token") || null;
-
-function getAuthHeader() {
-  return TOKEN ? { Authorization: `Bearer ${TOKEN}` } : {};
-}
