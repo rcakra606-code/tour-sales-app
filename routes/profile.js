9b1d2e964c855bff6800fb4f03ab74c454e11343
rcakra606-code <rcakra606@gmail.com>
2025-10-18 17:02:25 +0700

    Delete routes/profile.js

--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/profileController");
-
-router.get("/", controller.getProfile);
-router.put("/", controller.updateProfile);
-
-module.exports = router;
