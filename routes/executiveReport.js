bfcdabd64919111a2274341bc7ec329e4a8c4669
rcakra606-code <rcakra606@gmail.com>
2025-10-18 17:02:09 +0700

    Delete routes/executiveReport.js

--- routes/executiveReport.js
+++
@@ -1,8 +0,0 @@
-// routes/executiveReport.js
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/executiveReportController");
-
-router.get("/export/excel", controller.exportExecutiveReport);
-
-module.exports = router;
