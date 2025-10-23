23783cf1aab73b3fa5404d6f1ce01b380a8cc836
rcakra606-code <rcakra606@gmail.com>
2025-10-23 10:38:50 +0700

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,13 +1,10 @@
 import express from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import {
-  getDashboardSummary,
-  getTourRegionData,
-} from "../controllers/dashboardController.js";
+import { getDashboardSummary, getTourRegionStats } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
 router.get("/summary", authenticate, getDashboardSummary);
-router.get("/tour-region", authenticate, getTourRegionData);
+router.get("/tour-region", authenticate, getTourRegionStats);
 
 export default router;
