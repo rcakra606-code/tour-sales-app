487732f171909307e00c3f5b080f9b74ca2ef503
rcakra606-code <rcakra606@gmail.com>
2025-10-23 19:31:23 +0700

    Update regions.js

--- routes/regions.js
+++ routes/regions.js
@@ -1,17 +1,9 @@
 import express from "express";
-import { authenticate, authorizeAdmin } from "../middleware/authMiddleware.js";
-import {
-  getRegions,
-  createRegion,
-  updateRegion,
-  deleteRegion,
-} from "../controllers/regionController.js";
+import { getRegions, createRegion, deleteRegion } from "../controllers/regionController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
 router.get("/", authenticate, getRegions);
-router.post("/", authenticate, authorizeAdmin, createRegion);
-router.put("/:id", authenticate, authorizeAdmin, updateRegion);
-router.delete("/:id", authenticate, authorizeAdmin, deleteRegion);
-
+router.post("/", authenticate, createRegion);
+router.delete("/:id", authenticate, deleteRegion);
 export default router;
